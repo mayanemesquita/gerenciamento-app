7ea8d4c863d20b3b59b5a5f54881de21ccd5c420
mayanemesquitta <mayanemesquitta@uninove.edu.br>
2020-05-02 10:38:41 -0300

    Initial commit

---
+++ src/main/java/br/com/algaworks/gerenciamentoapp/GerenciamentoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.gerenciamentoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/algaworks/gerenciamentoapp/GerenciamentoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.gerenciamentoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
